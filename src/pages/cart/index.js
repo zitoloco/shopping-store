b471b34309b0c32e2f6beb41e2c22a1a1c9f8190
Leo Ribas <leonas.ribas@gmail.com>
2019-03-01 04:45:31 -0300

    feat: placed Header component on both cart and main pages

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
 
 export default class Cart extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1>Cart Page</h1>
-        <Link to='/'>Main</Link>
       </div>
     )
   }
