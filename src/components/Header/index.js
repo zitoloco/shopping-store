81ef006c0ae8a1eb918385fc9a1468013f787c5e
Leo Ribas <leonas.ribas@gmail.com>
2019-03-03 02:58:27 -0300

    feat: added cart tracker component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import CartCounter from '../CartCounter';
 export default class Header extends Component {
   render() {
     return (
@@ -8,6 +8,7 @@ export default class Header extends Component {
           <Link to='/'>Main</Link>
           <h1>Header</h1>
           <Link to='/cart'>Cart</Link>
+          <CartCounter />
       </div>
     )
   }
